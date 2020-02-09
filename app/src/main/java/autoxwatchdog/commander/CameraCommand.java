00741b407af99a3a6b61d9ced00aa429bce80092
Cavan Biggs <cbiggs7635@conestogac.on.ca>
2020-02-09 17:20:39 -0500

    Added needed activities

---
+++ app/src/main/java/autoxwatchdog/commander/CameraCommand.java
@@ -0,0 +1,14 @@
+package autoxwatchdog.commander;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CameraCommand extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_camera_command);
+    }
+}
