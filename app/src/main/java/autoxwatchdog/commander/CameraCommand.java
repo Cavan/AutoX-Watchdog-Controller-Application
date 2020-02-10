770e50ce3d0256081e0106c6cd2889a13e507241
Cavan Biggs <cbiggs7635@conestogac.on.ca>
2020-02-10 12:58:40 -0500

    App is now able to send commands to hardware unit

--- app/src/main/java/autoxwatchdog/commander/CameraCommand.java
+++ app/src/main/java/autoxwatchdog/commander/CameraCommand.java
@@ -2,13 +2,60 @@ package autoxwatchdog.commander;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.telephony.SmsManager;
+import android.view.View;
+import android.widget.Toast;
 
 public class CameraCommand extends AppCompatActivity {
+
+    private SmsManager smgr;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_camera_command);
+        //Initialize the sms manager variable
+        smgr = SmsManager.getDefault();
     }
+
+
+
+    public void sendFrontCaptureCommand(View view) {
+        buttonTest("Front Image Requested: " + getString(R.string.capture_front) );
+        sendCommandToHardware(getString(R.string.capture_front));
+    }
+
+    public void sendLeftCaptureCommand(View view) {
+        buttonTest("Left Image Requested: " + getString(R.string.capture_left));
+        sendCommandToHardware(getString(R.string.capture_left));
+    }
+
+    public void sendRightCaptureCommand(View view) {
+        buttonTest("Right Image Requested: " + getString(R.string.capture_right));
+        sendCommandToHardware(getString(R.string.capture_right));
+    }
+
+    public void sendRearCaptureCommand(View view) {
+        buttonTest("Rear Image Requested: " + getString(R.string.capture_rear));
+        sendCommandToHardware(getString(R.string.capture_rear));
+    }
+
+    private void buttonTest(String buttonPressed){
+        Context context = getApplicationContext();
+        CharSequence text = buttonPressed;
+        int duration = Toast.LENGTH_SHORT;
+
+        Toast toast = Toast.makeText(context, text, duration);
+        toast.show();
+    }
+
+    //Send the command to the hardware
+    private void sendCommandToHardware(String command){
+
+        smgr.sendTextMessage(getString(R.string.hardware_unit), null, command, null, null);
+
+    }
+
 }
