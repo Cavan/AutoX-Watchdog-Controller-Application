faa1af14d9a88dd632a4a0ce8f445bbb02adf0ac
Cavan Biggs <cbiggs7635@conestogac.on.ca>
2020-02-10 22:52:27 -0500

    Adding notification for incomming ALERTS

--- app/src/main/java/autoxwatchdog/commander/MainActivity.java
+++ app/src/main/java/autoxwatchdog/commander/MainActivity.java
@@ -1,17 +1,27 @@
 package autoxwatchdog.commander;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NotificationCompat;
 
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final String PRIMARY_CHANNEL_ID = "primary_notification_channel";
+    private NotificationManager mNotifyManager;
+    private static final int NOTIFICATION_ID = 0;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        createNotificationChannel();
     }
 
     public void testCommands(View view) {
@@ -19,4 +29,39 @@ public class MainActivity extends AppCompatActivity {
         startActivity(intent);
 
     }
+
+    //Create Notification for Alerts
+    public void sendNotification(){
+        NotificationCompat.Builder notifyBuilder = getNotificationBuilder();
+        mNotifyManager.notify(NOTIFICATION_ID, notifyBuilder.build());
+    }
+
+    public void createNotificationChannel()
+    {
+        mNotifyManager = (NotificationManager)
+                getSystemService(NOTIFICATION_SERVICE);
+        if(android.os.Build.VERSION.SDK_INT >=
+                                    android.os.Build.VERSION_CODES.O){
+            //Create notification channel
+            NotificationChannel notificationChannel = new NotificationChannel(PRIMARY_CHANNEL_ID,
+                    "Object Motion Detected", NotificationManager.IMPORTANCE_HIGH);
+            notificationChannel.enableLights(true);
+            notificationChannel.setLightColor(Color.RED);
+            notificationChannel.enableVibration(true);
+            notificationChannel.setDescription("AutoX Watchdog: Motion Detected!!!");
+            mNotifyManager.createNotificationChannel(notificationChannel);
+
+        }
+    }
+
+    private NotificationCompat.Builder getNotificationBuilder(){
+        NotificationCompat.Builder notifyBuilder = new NotificationCompat.Builder(this, PRIMARY_CHANNEL_ID)
+                .setContentTitle("Object Motion Detected")
+                .setContentText("Motion has been detected near or around your vehicle")
+                .setSmallIcon(R.drawable.ic_car_icon);
+
+            return notifyBuilder;
+
+    }
+
 }
