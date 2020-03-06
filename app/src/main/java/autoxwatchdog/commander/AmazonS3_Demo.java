c1a362cc835741f3eb59d13aa3b4b3907b5cbdce
Cavan Biggs <cbiggs7635@conestogac.on.ca>
2020-03-06 16:29:52 -0500

    The MMS function on the hardware is not working, we are now going to be uploading our images to an amazon S3 bucket, and retrieving them from that same bucket.

---
+++ app/src/main/java/autoxwatchdog/commander/AmazonS3_Demo.java
@@ -0,0 +1,14 @@
+package autoxwatchdog.commander;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AmazonS3_Demo extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_amazon_s3__demo);
+    }
+}
--- app/src/main/java/autoxwatchdog/commander/AutoXImageViewer.java
+++ app/src/main/java/autoxwatchdog/commander/AutoXImageViewer.java
@@ -3,6 +3,9 @@ package autoxwatchdog.commander;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class AutoXImageViewer extends AppCompatActivity {
 
@@ -10,5 +13,16 @@ public class AutoXImageViewer extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_auto_ximage_viewer);
+        //Custom code for displaying images
+        final ListView list = findViewById(R.id.mmsList);
+        ArrayList<SubjectData> arrayList = new ArrayList<SubjectData>();
+        arrayList.add(new SubjectData("JAVA", "https://www.tutorialspoint.com/java/", "https://www.tutorialspoint.com/java/images/java-mini-logo.jpg"));
+        arrayList.add(new SubjectData("Python", "https://www.tutorialspoint.com/python/", "https://www.tutorialspoint.com/python/images/python-mini.jpg"));
+        arrayList.add(new SubjectData("Javascript", "https://www.tutorialspoint.com/javascript/", "https://www.tutorialspoint.com/javascript/images/javascript-mini-logo.jpg"));
+        arrayList.add(new SubjectData("Cprogramming", "https://www.tutorialspoint.com/cprogramming/", "https://www.tutorialspoint.com/cprogramming/images/c-mini-logo.jpg"));
+        arrayList.add(new SubjectData("Cplusplus", "https://www.tutorialspoint.com/cplusplus/", "https://www.tutorialspoint.com/cplusplus/images/cpp-mini-logo.jpg"));
+        arrayList.add(new SubjectData("Android", "https://www.tutorialspoint.com/android/", "https://www.tutorialspoint.com/android/images/android-mini-logo.jpg"));
+        CustomAdapter customAdapter = new CustomAdapter(this, arrayList);
+        list.setAdapter(customAdapter);
     }
 }
